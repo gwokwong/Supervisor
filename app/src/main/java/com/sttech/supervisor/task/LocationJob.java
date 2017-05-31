a4f5df4bfb9623558c279fa4343b5645c03009ce
wells <guowang1102@163.com>
2017-05-31 16:22:45 +0800

    登录超时处理

--- app/src/main/java/com/sttech/supervisor/task/LocationJob.java
+++ app/src/main/java/com/sttech/supervisor/task/LocationJob.java
@@ -55,63 +55,64 @@ public class LocationJob extends Job {
         mListener = new BDLocationListener() {
             @Override
             public void onReceiveLocation(final BDLocation location) {
-//                Logger.d("onReceiveLocation22");
-                if (null != location && location.getLocType() != BDLocation.TypeServerError) {
-                    location.getCity();
-                    location.getLatitude();
-                    StringBuffer sb = new StringBuffer(256);
-                    sb.append("\nlatitude : ");// 纬度
-                    sb.append(location.getLatitude());
-                    sb.append("\nlontitude : ");// 经度
-                    sb.append(location.getLongitude());
-                    sb.append("\ncity : ");// 城市
-                    sb.append(location.getCity());
-                    Logger.d("LocationJob定位成功" + sb);
-                    Logger.t("logo").d("定位success");
-                    locationService.unregisterListener(mListener);
-                    locationService.destroy();
-
-                    //TODO 判断和上次发送成功相差是否超过5分钟，如果超过就可以再次发送
-                    List<LocationInfo> locationInfos = SQLite.select().from(LocationInfo.class).where().orderBy(LocationInfo_Table.time, false).queryList();  //false 为降序
-                    long now = System.currentTimeMillis();
-                    if (locationInfos.size() > 0) {
-                        long time = now - locationInfos.get(0).time;
-                        if (time > LOCATION_DELAY_TIME - 1100) {
-                            LocationInfo locationInfo = new LocationInfo();
-                            locationInfo.name = "机主";
-                            locationInfo.tryTimes = 0;
-                            locationInfo.latitude = location.getLatitude();
-                            locationInfo.lontitude = location.getLongitude();
-                            locationInfo.time = now;
-                            locationInfo.save();
-                        }
-                    } else {
-                        LocationInfo locationInfo = new LocationInfo();
-                        locationInfo.name = "机主";
-                        locationInfo.tryTimes = 0;
-                        locationInfo.time = now;
-                        locationInfo.latitude = location.getLatitude();
-                        locationInfo.lontitude = location.getLongitude();
-                        locationInfo.save();
-                    }
-
-                } else {
-                    Logger.d("LocationJob定位失败" + TRY_TIMES);
-                    if (++TRY_TIMES >= MAX_TRY_TIME) {
+                if (null != location) {
+                    if (location.getLocType() != BDLocation.TypeServerError) {
+                        location.getCity();
+                        location.getLatitude();
+                        StringBuffer sb = new StringBuffer(256);
+                        sb.append("\nlatitude : ");// 纬度
+                        sb.append(location.getLatitude());
+                        sb.append("\nlontitude : ");// 经度
+                        sb.append(location.getLongitude());
+                        sb.append("\ncity : ");// 城市
+                        sb.append(location.getCity());
+                        Logger.d("LocationJob定位成功" + sb);
+                        Logger.t("logo").d("定位success");
                         locationService.unregisterListener(mListener);
                         locationService.destroy();
-                        //TODO  定位失败记录在数据库 最后一次尝试的时候记录
+
+                        //TODO 判断和上次发送成功相差是否超过5分钟，如果超过就可以再次发送
                         List<LocationInfo> locationInfos = SQLite.select().from(LocationInfo.class).where().orderBy(LocationInfo_Table.time, false).queryList();  //false 为降序
                         long now = System.currentTimeMillis();
-                        long time = now - locationInfos.get(0).time;
-                        if (time > LOCATION_DELAY_TIME - 1100) {
+                        if (locationInfos.size() > 0) {
+                            long time = now - locationInfos.get(0).time;
+                            if (time > LOCATION_DELAY_TIME - 1100) {
+                                LocationInfo locationInfo = new LocationInfo();
+                                locationInfo.name = "机主";
+                                locationInfo.tryTimes = 0;
+                                locationInfo.latitude = location.getLatitude();
+                                locationInfo.lontitude = location.getLongitude();
+                                locationInfo.time = now;
+                                locationInfo.save();
+                            }
+                        } else {
                             LocationInfo locationInfo = new LocationInfo();
                             locationInfo.name = "机主";
                             locationInfo.tryTimes = 0;
                             locationInfo.time = now;
-                            locationInfo.lontitude = 0;
-                            locationInfo.latitude = 0;
+                            locationInfo.latitude = location.getLatitude();
+                            locationInfo.lontitude = location.getLongitude();
                             locationInfo.save();
+                        }
+
+                    } else {
+                        Logger.d("LocationJob定位失败" + TRY_TIMES);
+                        if (++TRY_TIMES >= MAX_TRY_TIME) {
+                            locationService.unregisterListener(mListener);
+                            locationService.destroy();
+                            //TODO  定位失败记录在数据库 最后一次尝试的时候记录
+                            List<LocationInfo> locationInfos = SQLite.select().from(LocationInfo.class).where().orderBy(LocationInfo_Table.time, false).queryList();  //false 为降序
+                            long now = System.currentTimeMillis();
+                            long time = now - locationInfos.get(0).time;
+                            if (time > LOCATION_DELAY_TIME - 1100) {
+                                LocationInfo locationInfo = new LocationInfo();
+                                locationInfo.name = "机主";
+                                locationInfo.tryTimes = 0;
+                                locationInfo.time = now;
+                                locationInfo.lontitude = 0;
+                                locationInfo.latitude = 0;
+                                locationInfo.save();
+                            }
                         }
                     }
                 }
